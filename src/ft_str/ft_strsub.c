6d186b4098f73a91405ddadfbbcc17841b8dc2c3
Artem DAYRABEKOV <adayrabe@e1r1p18.unit.ua>
2019-02-01 15:08:30 +0200

    Initial commit

---
+++ src/ft_str/ft_strsub.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strsub.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adayrabe <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/11/07 20:25:29 by adayrabe          #+#    #+#             */
+/*   Updated: 2017/11/07 20:25:29 by adayrabe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include "libft.h"
+
+char	*ft_strsub(const char *s, unsigned int start, size_t len)
+{
+	char			*res;
+	unsigned int	i;
+
+	if (!s || ft_strlen(s) <= start)
+		return (NULL);
+	res = ft_strnew(len);
+	if (!res)
+		return (NULL);
+	i = 0;
+	while (i < len && s[start + i])
+	{
+		res[i] = s[start + i];
+		i++;
+	}
+	return (res);
+}
